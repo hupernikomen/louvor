c56607106efe780e444e5d71a7d400af576e6ce4
Wilson Ramos <hupcontato@gmail.com>
2023-02-09 13:06:50 -0300

    Final

--- escala.js
+++ escala.js
@@ -3,175 +3,209 @@ const header = document.querySelector('header')
 const modal = document.querySelector('.modal')
 const containerModal = document.querySelector('.containerModal')
 header.append("Fevereiro")
+var numDias = 0
 
 
-const membro = localStorage.getItem('@membro')
+const membros = ["Wilson", "Paulinha", "Duda", "Lidiane", "Edvan", "Edmilson", "Kelviane", "Warley", "Thiago", , "Wesley", "Thalyson", "André"]
 
 
-membro==null ? containerModal.style.display= "flex" :containerModal.style.display= "none"  
+const _membro = localStorage.getItem('@membro')
+
+
+_membro == null ? containerModal.style.display = "flex" : containerModal.style.display = "none"
 
 const escala = [
     {
-        "data": "25/12",
+        "data": "10/02",
         "culto": "Doutrina",
-        "louvores": ["Jó", "Celebrai"],
+        "louvores": ["Renova-me", "Digno de Glória"],
         "louvor_extra": "Ao único",
-        "vocais": ["Paulinha", "Duda", "Lais"],
+        "vocais": ["Edvan", "Duda", "Lais"],
         "instrumentistas": ["Thalyson", "Wesley"]
 
     },
     {
-        "data": "25/12",
+        "data": "12/02",
         "culto": "EBD",
-        "louvores": ["Jó", "Celebrai"],
+        "louvores": ["Cantor Cristão 545", "Autoridade e Poder"],
         "louvor_extra": "Todavia me Alegrarei",
-        "vocais": ["Paulinha", "Duda", "Outra"],
+        "vocais": ["Paulinha", "Kelviane", "Edvan"],
         "instrumentistas": ["Thalyson", "Wesley"]
 
     },
     {
-        "data": "25/12",
+        "data": "12/02",
         "culto": "Louvor e Pregação",
-        "louvores": ["Jó", "Celebrai"],
+        "louvores": ["Não há Deus maior", "Meu Mestre", "Jó"],
         "louvor_extra": "Todavia me Alegrarei",
-        "vocais": ["Paulinha", "Duda", "Outra"],
+        "vocais": ["Paulinha", "Kelviane", "Edmilson"],
         "instrumentistas": ["Thalyson", "Wesley"]
     },
     {
-        "data": "25/12",
+        "data": "17/02",
+        "culto": "Oração",
+        "louvores": ["Cria em Mim", "Alto Preço"],
+        "louvor_extra": "Ao único",
+        "vocais": ["Edvan", "Duda", "Lidiane"],
+        "instrumentistas": ["Thalyson", "Wesley"]
+
+    },
+    {
+        "data": "19/02",
+        "culto": "EBD",
+        "louvores": ["Cantor Cristão 545", "Eu navegarei"],
+        "louvor_extra": "Todavia me Alegrarei",
+        "vocais": ["Paulinha", "Laís", "Edvan"],
+        "instrumentistas": ["Thalyson", "Wesley"]
+
+    },
+    {
+        "data": "19/02",
+        "culto": "Louvor e Pregação",
+        "louvores": ["Vem esta é a hora", "Isaías 9", "Rendido Estou"],
+        "louvor_extra": "Todavia me Alegrarei",
+        "vocais": ["Edmilson", "Paulinha", "Laís"],
+        "instrumentistas": ["Thalyson", "Wesley"]
+    },
+    {
+        "data": "24/02",
         "culto": "Doutrina",
-        "louvores": ["Jó", "Celebrai"],
+        "louvores": ["Ao único", "Ele é exaltado"],
         "louvor_extra": "Ao único",
-        "vocais": ["Paulinha", "Duda", "Lais"],
-        "instrumentistas": ["Thalyson", "Wesley"]
+        "vocais": ["Edvan", "Lidiane", "Duda"],
+        "instrumentistas": ["Thalyson", "Wesley", "André"]
 
     },
     {
-        "data": "25/12",
+        "data": "26/02",
         "culto": "EBD",
-        "louvores": ["Jó", "Celebrai"],
+        "louvores": ["Cantor Cristão 545", "Bom estarmos aqui"],
         "louvor_extra": "Todavia me Alegrarei",
-        "vocais": ["Paulinha", "Duda", "Outra"],
+        "vocais": ["Paulinha", "Kelviane", "Edvan"],
         "instrumentistas": ["Thalyson", "Wesley"]
 
     },
     {
-        "data": "25/12",
+        "data": "26/02",
         "culto": "Louvor e Pregação",
-        "louvores": ["Jó", "Celebrai"],
+        "louvores": ["Maranata", "Poder pra salvar", "Em Espírito, Em Verdade", "Alvo mais que a neve", "Porque Ele vive"],
         "louvor_extra": "Todavia me Alegrarei",
-        "vocais": ["Paulinha", "Duda", "Outra"],
-        "instrumentistas": ["Thalyson", "Wesley"]
-    },
-    {
-        "data": "25/12",
-        "culto": "Doutrina",
-        "louvores": ["Jó", "Celebrai"],
-        "louvor_extra": "Ao único",
-        "vocais": ["Paulinha", "Duda", "Lais"],
-        "instrumentistas": ["Thalyson", "Wesley"]
-
-    },
-    {
-        "data": "25/12",
-        "culto": "EBD",
-        "louvores": ["Jó", "Celebrai"],
-        "louvor_extra": "Todavia me Alegrarei",
-        "vocais": ["Paulinha", "Duda", "Outra"],
-        "instrumentistas": ["Thalyson", "Wesley"]
-
-    },
-    {
-        "data": "25/12",
-        "culto": "Louvor e Pregação",
-        "louvores": ["Jó", "Celebrai"],
-        "louvor_extra": "Todavia me Alegrarei",
-        "vocais": ["Paulinha", "Duda", "Outra"],
+        "vocais": ["Paulinha", "Edmilson", "Lidiane"],
         "instrumentistas": ["Thalyson", "Wesley"]
     },
 ]
 
 
-const membros = ["Wilson", "Paulinha", "Outro"]
 
 
 membros.map((membro) => {
+
     const button = document.createElement('button')
-    button.className ='btn_membro'
+    button.className = 'btn_membro'
     button.innerHTML = membro
     modal.append(button)
 
 
+
 })
 
-const arrBtn =document.querySelectorAll('.btn_membro')
+const arrBtn = document.querySelectorAll('.btn_membro')
 
 
 arrBtn.forEach((btn) => {
-    btn.addEventListener('click', ()=> {
+    btn.addEventListener('click', () => {
 
+        const load = document.querySelector('.containerloader')
+
+        load.style.display = 'flex'
+        
         localStorage.setItem('@membro', btn.innerHTML)
         containerModal.style.display = 'none'
-
+        
+        window.location.reload(true);
+        MontaEscala()
     })
-
+    
 })
 
-
-escala.map((item) => {
-
-    const escalaItem = `
+function Limpar() {
     
-    <div class="containerItem ${item.culto == "EBD" ? "ebd" :
-            item.culto == "Doutrina" ? "doutrina" :
-                item.culto == "Louvor e Pregação" ? "louvorepregacao" :
-                    "outro"
-        }">
-    <div class="ref">
-            <div>
-            <span class="data">${item.data}</span>
-                <span>${item.culto}</span>
+    localStorage.removeItem("@membro");
+    window.location.reload(true);
+}
+
+function MontaEscala() {
+
+
+    escala.map((item) => {
+
+        const instrumentista = item.instrumentistas.find((inst) => inst == _membro)
+        const vocal = item.vocais.find((inst) => inst == _membro)
+
+
+        const escalaItem = `
+    
+        <div class="containerItem ${item.culto == "EBD" ? "ebd" :
+                item.culto == "Doutrina" ? "doutrina" :
+                    item.culto == "Louvor e Pregação" ? "louvorepregacao" :
+                        item.culto == "Oração" ? "oracao" :
+                            "outro"
+            }">
+            <div class="ref">
+                <div>
+                    <span class="data">${item.data}</span>
+                    <span>${item.culto}</span>
+                </div>
             </div>
-        </div>
 
-        <div class="vocais">
-            <span>Vocais:</span>
-            <div>
-        ${item.vocais.map((vocal) => {
-            return `<span>${vocal}</span>`
-        })
-        }
-
+                <div class="vocais">
+                <span>Vozes:</span>
+                
+                <div>
+                    ${item.vocais.map((vocal) => {
+                return `<span class=${vocal == _membro && "me"}>${vocal}</span>`
+            })}
+                </div>
             </div>
-        </div>
 
-        <div class="instrumentos">
-        <span>Instrumentistas:</span>
-        <div>
-        ${item.instrumentistas.map((instrumentista) => {
-        return `<span>${instrumentista}</span>`
-    })
-    }
-
-        </div>
-        </div>
+                    <div class="instrumentos">
+                    <span>Instrumentos:</span>
+                    <div>
+                    ${item.instrumentistas.map((instrumentista) => {
+                return `<span class=${instrumentista == _membro && "me"}>${instrumentista}</span>`
+            })
+            }
+                
+                </div>
+                </div>
         <div class="louvores">
             <span>Louvores:</span>
             <div>
             ${item.louvores.map((louvor) => {
-            return `<span>${louvor}</span>`
-        })
+                return `<span>${louvor}</span>`
+            })
+            }
+        
+            
+            </div>
+            </div>
+            <span class="extra">Música Reserva: [ ${item.louvor_extra} ]</span>
+            </div>
+        
+        `
+
+
+
+        if (vocal || instrumentista) {
+            numDias++
+            main.innerHTML += escalaItem
         }
 
-            </div>
-            <span class="extra">[ ${item.louvor_extra} ]</span>
-        </div>
-
-        </div>
-
-    `
-
-    main.innerHTML += escalaItem
+        document.querySelector('.info').innerHTML = `${_membro} - ${numDias} dias de louvor`
 
 
-})
+
+    })
+
+
+}
