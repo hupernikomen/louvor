bd6906c9039177a5927ea016738a08b11f9588ab
Wilson Ramos <hupcontato@gmail.com>
2023-08-01 13:51:42 -0300

    Novas

--- filtro.js
+++ filtro.js
@@ -15,7 +15,7 @@ const escala = [
     "louvor": "Em Espírito em verdade",
     "escala_musical": "F#m",
     "equipe": ["Edvan", "Edmilson", "Kelviane", "Fernanda", "Paulinha", "Annes", "André", "Wesley"],
-    "avaliacao": 4
+    "avaliacao": 5
   },
   {
     "louvor": "Pra sempre",
@@ -48,39 +48,15 @@ const escala = [
     "avaliacao": 1
   },
   {
-    "louvor": "Quero que valorise",
-    "escala_musical": "",
-    "equipe": [],
-    "avaliacao": null
-  },
-  {
-    "louvor": "Cria em mim",
-    "escala_musical": "",
-    "equipe": ["Edmilson", "Edvan", "Paulinha", "Laís"],
-    "avaliacao": 1
-  },
-  {
-    "louvor": "Jó",
-    "escala_musical": "",
-    "equipe": ["Paulinha", "Laís", "Edmilson", "Edvan"],
-    "avaliacao": null
-  },
-  {
-    "louvor": "Vem esta é a hora",
-    "escala_musical": "",
-    "equipe": ["Edvan", "Lidiane", "Kelviane", "Laís"],
-    "avaliacao": null
-  },
-  {
     "louvor": "CC 154 - Firme nas promessas",
     "escala_musical": "F",
-    "equipe": ["Paulinha", "Edvam", "Edmilson", "Lidiane", "Laís"],
+    "equipe": ["Paulinha", "Edvan", "Edmilson", "Lidiane", "Laís"],
     "avaliacao": 3
   },
   {
     "louvor": "CC 46 - Jesus me transformou",
     "escala_musical": "G",
-    "equipe": ["Paulinha", "Edvam", "Edmilson", "Lidiane", "Laís"],
+    "equipe": ["Paulinha", "Edvan", "Edmilson", "Lidiane", "Laís"],
     "avaliacao": 3
   },
   {
@@ -95,19 +71,54 @@ const escala = [
     "equipe": ["Paulinha"],
     "avaliacao": ""
   },
+  {
+    "louvor": "Maranata",
+    "escala_musical": "Bm",
+    "equipe": ["Fernanda", "Paulinha", "Laís", "Wilson", "André", "Annes", "Wesley"],
+    "avaliacao": 4
+  },
+  {
+    "louvor": "Vem, esta é a hora - Vineyard",
+    "escala_musical": "D",
+    "equipe": ["Fernanda", "Paulinha", "Laís", "Wilson", "André", "Annes", "Wesley"],
+    "avaliacao": 4
+  },
+  {
+    "louvor": "Cria em mim",
+    "escala_musical": "C",
+    "equipe": ["Fernanda", "Paulinha", "Laís", "Wilson", "André", "Annes", "Wesley"],
+    "avaliacao": 4
+  },
+  {
+    "louvor": "Jó",
+    "escala_musical": "",
+    "equipe": ["Edvan", "Edmilson", "Kelviane", "Fernanda", "Paulinha", "Annes", "André", "Wesley"],
+    "avaliacao": 5
+  },
+  {
+    "louvor": "Jesus é o caminho",
+    "escala_musical": "",
+    "equipe": ["Edvan", "Edmilson", "Kelviane", "Fernanda", "Paulinha", "Annes", "André", "Wesley"],
+    "avaliacao": 5
+  },
 ]
 
 
 
 const equipes = [
-  ["Fernanda", "Duda", "Edmilson", "Edvan"], //1
-  ["Fernanda", "Laís", "Edmilson", "Edvan"], //2
-  ["Fernanda", "Kelviane", "Edmilson", "Edvan"], //3
-  ["Fernanda", "Lidiane", "Edmilson", "Edvan"], //4
-  ["Paulinha", "Duda", "Edmilson", "Edvan"], //5
-  ["Paulinha", "Laís", "Edmilson", "Edvan"], //6
-  ["Paulinha", "Kelviane", "Edmilson", "Edvan"], //7 
-  ["Paulinha", "Lidiane", "Edmilson", "Edvan"], //8
+  ["Fernanda", "Duda", "Edmilson", "Edvan"], // 0
+  ["Paulinha", "Laís", "Edmilson", "Edvan"], // 1
+  ["Fernanda", "Kelviane", "Edmilson", "Edvan"], // 2 ----> Parei aqui
+  ["Paulinha", "Lidiane", "Edmilson", "Edvan"], // 3
+  ["Paulinha", "Duda", "Edmilson", "Edvan"], // 4
+  ["Fernanda", "Laís", "Edmilson", "Edvan"], // 5
+  ["Paulinha", "Kelviane", "Edmilson", "Edvan"], // 6
+  ["Fernanda", "Lidiane", "Edmilson", "Edvan"], // 7
+  ["Edvan","Kelviane","Lais","Lidiane","Andre","Warley","Wesley"], //8 //SEXTA
+  ["Edmilson",
+  "Edvan",
+  "Lidiane",
+  "Duda",] //9 Essa pode ser montada da forma que eu quizer
 ]
 
 
@@ -125,24 +136,12 @@ function BuscarEquipePorLouvor(louvor) {
 }
 
 
-function BuscarMusicasMedia(media) {
-  escala.filter((item) => {
-    if (item.louvor.avaliacao >= media) {
-      console.log({
-        nome: item.louvor.nome,
-        escala_musical: item.louvor.escala_musical,
-        avaliacao: item.louvor.avaliacao
-      })
-    }
-  })
-}
 
-
-async function BuscarMusicaPorEquipe(arrVocal, avaliacao) {
+function BuscarMusicaPorEquipe(arrVocal, avaliacao) {
 
 
   console.log(`\n`)
-  console.log("Equipe: ", arrVocal)
+  console.log("Vocais: ", ...Object.values(arrVocal))
 
   escala.filter((item) => {
 
@@ -150,13 +149,10 @@ async function BuscarMusicaPorEquipe(arrVocal, avaliacao) {
       return item.equipe.indexOf(vocal) > -1 && item.avaliacao >= avaliacao
     })
 
-
     if (encontraVocal.every((item) => item === true)) {
-      console.log({
-        louvor: item.louvor,
-        escala_musical: item.escala_musical,
-        avaliacao: item.avaliacao
-      })
+      console.log(
+        item.louvor + " - " + item.escala_musical + " - " + item.avaliacao
+      )
     }
   })
 }
@@ -164,15 +160,23 @@ async function BuscarMusicaPorEquipe(arrVocal, avaliacao) {
 // Pra executar busca teclar F1 e cucar em "Run Code"
 
 
-equipes.map((equipe) => {
-  BuscarMusicaPorEquipe(equipe, 3)
-})
+//VER TODAS AS EQUIPES
+
+// equipes.map((equipe) => {
+//   BuscarMusicaPorEquipe(equipe, 3)
+// })
 
 
 
-// Passar a média desejada
-// BuscarMusicasMedia(3)
+
+// BUSCAR APENAS 1 EQUIPE
+
+BuscarMusicaPorEquipe(equipes[9], 3)
 
 
-// Passar o louvor
+
+
+
+// BUSCAR EQUIPES POR LOUVOR
+
 // BuscarEquipePorLouvor("Não há Deus maior")
